1f4a68a0d17dd4e28ff6d91bee50b26a8d47d724
botmirko123 <anyamanni072@gmail.com>
2024-08-18 19:51:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['xxxxxxxxxx', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
+  ['393201391411', 'spidy💋', true],
   ['xxxxxxxxxx',],
   ['xxxxxxxxxx'],	
   ['xxxxxxxxxx',],
